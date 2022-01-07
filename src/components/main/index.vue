87de2b9394fc75e6c243fdac940303e429c91527
Caocao12138 <82129812+Caocao12138@users.noreply.github.com>
2022-01-07 15:11:44 +0800

    Initial commit

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -2,7 +2,7 @@
   <div class="page-index">
     <el-row>
       <el-col :span="5"> <left-menu /> </el-col>
-      <el-col :span="19">轮播图及右侧</el-col>
+      <el-col :span="19"><right-main-life /></el-col>
     </el-row>
     <el-row>
       <el-col :span="24">猫眼电影</el-col>
@@ -12,10 +12,12 @@
 
 <script>
 import leftMenu from "./components/menu.vue";
+import rightMainLife from "./components/life.vue";
 export default {
   name: "main-container", // 中间区域
   components: {
     leftMenu,
+    rightMainLife,
   },
 };
 </script>
